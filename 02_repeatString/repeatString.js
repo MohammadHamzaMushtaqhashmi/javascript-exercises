03234158ae09c538f53dc22ffbbd0fb527417e47
HaMza HashMi <73049209+MohammadHamzaMushtaqhashmi@users.noreply.github.com>
2022-08-18 04:46:17 +0500

    all test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newstr = "";
+    if(num < 0){
+        return "ERROR"
+    }
+    if(str === "" || num === 0){
+        return ""
+    }
+    for(let i=0; i<num; i++){
+        newstr += str;
+    }
+    return newstr
 
 };
 
